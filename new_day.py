9378fe305709c91a1a8a7fdb85d725447048334e
William Y. Feng <womogenes2@gmail.com>
2022-12-01 21:26:11 -0800

    Better auto submission

--- new_day.py
+++ new_day.py
@@ -3,6 +3,7 @@ import os
 
 import datetime as dt
 day = (dt.datetime.now() + dt.timedelta(days=1, hours=4, minutes=30)).day
+year = dt.datetime.now().year
 
 prefix = f"day_{str(day).zfill(2)}"
 
@@ -13,8 +14,14 @@ if not prefix in os.listdir():
         fout.write(f"""# from aocd import lines, submit
 
 
-# submit(ans)
+# submit(ans, part="a", day={day}, year={year})
 """)
 
-    open(f"./{prefix}_p2.py", "w")
+    with open(f"./{prefix}_p2.py", "w") as fout:
+        fout.write(f"""# from aocd import lines, submit
+
+
+# submit(ans, part="b", day={day}, year={year})
+""")
+
     open(f"./{prefix}_sample.in", "w")
