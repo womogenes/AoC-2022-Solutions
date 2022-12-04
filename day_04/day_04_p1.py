b69a8344aa617451a215cf27177c11c55c16bf0b
William Y. Feng <womogenes2@gmail.com>
2022-12-03 21:35:16 -0800

    Finish day 4

---
+++ day_04/day_04_p1.py
@@ -0,0 +1,17 @@
+with open("./day_04.in") as fin:
+    lines = fin.read().strip().split()
+
+
+ans = 0
+for line in lines:
+    elves = line.split(",")
+    ranges = [list(map(int, elf.split("-"))) for elf in elves]
+
+    a, b = ranges[0]
+    c, d = ranges[1]
+
+    if a <= c and b >= d or a >= c and b <= d:
+        ans += 1
+
+
+print(ans)
